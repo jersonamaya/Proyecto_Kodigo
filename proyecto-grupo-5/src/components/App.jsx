8ea2708e78c7fc9acb685f4ea62d9d6643c4abde
giovanni-rivas <132028431+GiovanniRivas@users.noreply.github.com>
2023-08-29 01:18:52 -0600

    creamos los componentes principales

---
+++ proyecto-grupo-5/src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Router, BrowserRouter, Routes, Route  } from "react-router-dom";
+import Home from './Home'
+import About from './About'
+import NotFound from "./NotFound";
+import Menu from "./Menu";
+function App(){
+    return(
+        <div>
+            <BrowserRouter>
+                <Menu/>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About/>}/>
+
+                    <Route path="*" element={<NotFound />} />
+                   
+                   
+                </Routes>
+            </BrowserRouter> 
+            </div>
+             )
+            
+} 
+export default App
