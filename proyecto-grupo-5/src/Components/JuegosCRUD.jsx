7a0fe5266218bceec7c04ca40fe787911cd12a9a
carlossosa-1998 <113155435+carlossosa-1998@users.noreply.github.com>
2023-08-30 11:47:29 -0600

    CRUD con modales, en funcionamiento.

---
+++ proyecto-grupo-5/src/Components/JuegosCRUD.jsx
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react'
+import Menu from './Menu'
+import axios from "axios"
+import Tabla from './Tabla'
+import JuegosFORM from "./JuegosFORM"
+
+const JuegosCRUD = () => {
+  
+  const[juegos, setJuegos] = useState();
+  const[id, setId] = useState()
+  const[del, setDel] = useState()
+
+  function configurar(id, del){
+    setId(id)
+    setDel(del)
+  }
+
+  useEffect(() => {
+    cargarJuegos()
+  }, [])
+
+  async function cargarJuegos(){
+    try{
+      let res = await axios("https://denny2023.azurewebsites.net/api/juegos");
+      let data = await res.data;
+
+      setJuegos(data);
+    }
+    catch(error){
+      alert(error);
+    }
+  }
+
+  return (
+    <div>
+        <Menu />
+        {
+          juegos === undefined ?
+          <div>
+            <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+            <h2>Cargando...</h2>
+          </div>
+          :
+          <Tabla lista={juegos} evento={configurar} controlador="juegos" cols={["Juego ID", "Título", "Descripcion", "Plataforma", "Precio", "Categoría"]} />
+        }
+
+        <div className="modal fade" id="juegosModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+              <div className="modal-content">
+              <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">Formulario Juego</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                  <JuegosFORM Form id={id} del={del} actualizar={cargarJuegos} />
+              </div>
+              </div>
+          </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default JuegosCRUD;
--- proyecto-grupo-5/src/index.js
+++ proyecto-grupo-5/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './Components/App';
 import './index.css';
-import App from './components/App';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <App></App>
-  
+    <App />
   </div>
-  
 );
-
